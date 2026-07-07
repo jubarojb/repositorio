8d36e49ec1621a5fe88c9a421c43a2abb8566d76
jubarojb <jubarojb@gmail.com>
2026-07-07 09:33:33 +0200

    Delete includes.js

--- includes.js
+++
@@ -1,8 +0,0 @@
-async function loadInclude(id, file) {
-    const response = await fetch(file);
-    const html = await response.text();
-    document.getElementById(id).innerHTML = html;
-}
-
-loadInclude('header', 'http://localhost/web/includes/header.html');
-loadInclude('footer', 'http://localhost/web/includes/footer.html');
